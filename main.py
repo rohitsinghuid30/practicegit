00c5c768a97c3de4f828641af2be57b712863197
Rohit Singh <rohituid30@gmail.com>
2024-08-16 06:55:09 +0530

    flask code added

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 # This is our main app
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Welcome to the flask coding."
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
